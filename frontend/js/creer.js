9e5a7e89cd0913bce328a0e3e5e14e60905fd2a8
zoebocbig <zoebocquet26@gmail.com>
2026-02-03 09:45:36 +0100

    modif creer

--- frontend/js/creer.js
+++ frontend/js/creer.js
@@ -1,52 +1,18 @@
-const button = document.getElementById("createBtn");
-const message = document.getElementById("message");
+document.addEventListener('DOMContentLoaded', () => {
+    const btnQuiz = document.getElementById('btnQuiz');
+    const btnQuizDoc = document.getElementById('btnQuizDoc');
 
-const API_URL = "http://backend:5000/api";
-const API_KEY = "TA_CLE_API_ICI";
-
-button.addEventListener("click", async () => {
-    try {
-        const res = await fetch(`${API_URL}/create-room-from-doc`, {
-            method: "POST",
-            headers: {
-                "x-api-key": API_KEY
-            }
+    if(btnQuiz) {
+        btnQuiz.addEventListener('click', () => {
+            alert("Tu vas créer un quiz simple !");
+            
         });
-
-        const data = await res.json();
-
-        if (!res.ok) {
-            message.style.color = "red";
-            message.textContent = data.message;
-            return;
-        }
-
-        message.style.color = "green";
-        message.textContent = `Quiz créé ! Code : ${data.roomCode}`;
-
-
-    } catch (err) {
-        message.style.color = "red";
-        message.textContent = "Serveur indisponible";
-        console.error(err);
     }
-});
 
-document.getElementById("creerDoc").addEventListener("click", async () => {
-    const response = await fetch("http://localhost:3000/api/create-room-from-doc", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": "TA_CLE_API_ICI"
-        },
-        body: JSON.stringify({
-            questions: [
-                { question: "Capitale de la France ?", answer: "Paris" },
-                { question: "2 + 2 ?", answer: "4" }
-            ]
-        })
-    });
-
-    const data = await response.json();
-    alert("Code du quiz : " + data.roomCode);
+    if(btnQuizDoc) {
+        btnQuizDoc.addEventListener('click', () => {
+            alert("Tu vas créer un quiz avec document !");
+            
+        });
+    }
 });
